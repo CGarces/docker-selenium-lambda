5907486da8e64468262d82e5d50e4ed8501033bc
Carlos Garcés <CGarces@users.noreply.github.com>
2022-10-31 12:24:23 +0100

    Adapted to AWS Lambda using SAM

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,19 @@
-FROM public.ecr.aws/lambda/python@sha256:e9cc22ac03cdf72bb61f92b65bc65e725564eddaa3af518968271e51bbe69198 as build
+FROM public.ecr.aws/lambda/python:3.9 as build
 RUN yum install -y unzip && \
     curl -Lo "/tmp/chromedriver.zip" "https://chromedriver.storage.googleapis.com/106.0.5249.61/chromedriver_linux64.zip" && \
     curl -Lo "/tmp/chrome-linux.zip" "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1036826%2Fchrome-linux.zip?alt=media" && \
     unzip /tmp/chromedriver.zip -d /opt/ && \
     unzip /tmp/chrome-linux.zip -d /opt/
 
-FROM public.ecr.aws/lambda/python@sha256:e9cc22ac03cdf72bb61f92b65bc65e725564eddaa3af518968271e51bbe69198
+FROM public.ecr.aws/lambda/python:3.9
 RUN yum install atk cups-libs gtk3 libXcomposite alsa-lib \
     libXcursor libXdamage libXext libXi libXrandr libXScrnSaver \
     libXtst pango at-spi2-atk libXt xorg-x11-server-Xvfb \
-    xorg-x11-xauth dbus-glib dbus-glib-devel -y
-RUN pip install selenium
+    xorg-x11-xauth dbus-glib dbus-glib-devel -y && \
+    yum clean all && \
+    rm -rf /var/cache/yum
+RUN pip install selenium --target "${LAMBDA_TASK_ROOT}"
 COPY --from=build /opt/chrome-linux /opt/chrome
 COPY --from=build /opt/chromedriver /opt/
-COPY main.py ./
+COPY main.py ${LAMBDA_TASK_ROOT}
 CMD [ "main.handler" ]
--- main.py
+++ main.py
@@ -2,23 +2,29 @@ from selenium import webdriver
 from tempfile import mkdtemp
 from selenium.webdriver.common.by import By
 
+options = webdriver.ChromeOptions()
+options.binary_location = "/opt/chrome/chrome"
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1280x1696")
+options.add_argument("--single-process")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-dev-tools")
+options.add_argument("--no-zygote")
+options.add_argument(f"--user-data-dir={mkdtemp()}")
+options.add_argument(f"--data-path={mkdtemp()}")
+options.add_argument(f"--disk-cache-dir={mkdtemp()}")
+options.add_argument("--remote-debugging-port=9222")
+
 
 def handler(event=None, context=None):
-    options = webdriver.ChromeOptions()
-    options.binary_location = '/opt/chrome/chrome'
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1280x1696")
-    options.add_argument("--single-process")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-dev-tools")
-    options.add_argument("--no-zygote")
-    options.add_argument(f"--user-data-dir={mkdtemp()}")
-    options.add_argument(f"--data-path={mkdtemp()}")
-    options.add_argument(f"--disk-cache-dir={mkdtemp()}")
-    options.add_argument("--remote-debugging-port=9222")
-    chrome = webdriver.Chrome("/opt/chromedriver",
-                              options=options)
+    chrome = webdriver.Chrome("/opt/chromedriver", options=options)
     chrome.get("https://example.com/")
-    return chrome.find_element(by=By.XPATH, value="//html").text
+
+    noticias = chrome.find_elements(by=By.XPATH, value="//h2[@class='c_t ']/a[2]")
+    for x in range(len(noticias)):
+        print(noticias[x].text)
+
+    chrome.quit()
+    return "OK"
